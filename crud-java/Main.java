6dcb91b010fb5c49dc0a717a01190e783cefcc45
Caio Souza <caioss20.cs@gmail.com>
2026-05-04 16:29:20 -0300

    refactor: refatorando código

--- crud-java/Main.java
+++ crud-java/Main.java
@@ -4,78 +4,24 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Produto> listaProdutos = new ArrayList<>();
+        ProdutoService service = new ProdutoService();
 
         Produto p1 = new Produto();
         Produto p2 = new Produto();
+
         p1.id = 1;
-        p1.nome = "Capa de celular";
-        p1.preco = 9.99;
-        p1.quantidade = 4;
+        p1.nome = "Caneta Azul";
+        p1.preco = 2.99;
+        p1.quantidade = 100;
 
         p2.id = 2;
-        p2.nome = "Pelicula";
-        p2.preco = 5.99;
-        p2.quantidade = 2;
+        p2.nome = "Caneta Vermelha";
+        p2.preco = 2.99;
+        p2.quantidade = 100;
 
-        listaProdutos.add(p1);
-        listaProdutos.add(p2);
+        service.adicionarProduto(p1);
+        service.adicionarProduto(p2);
 
-        // for (Produto produto : listaProdutos) {
-        // produto.exibirDetalhes();
-        // }
-
-        String nomeBusca = "Pelicula";
-
-        boolean nomeEncontrado = false;
-
-        for (Produto produto : listaProdutos) {
-
-            if (produto.nome.equals(nomeBusca)) {
-                System.out.println(produto.id);
-                System.out.println(produto.nome);
-                System.out.println(produto.preco);
-                System.out.println(produto.quantidade);
-
-                nomeEncontrado = true;
-
-            }
-
-        }
-
-        if (!nomeEncontrado) {
-            System.out.println("Nome não encontrado na lista");
-        }
-
-        int buscaId = 2;
-        boolean atualizado = false;
-
-        for (Produto produto : listaProdutos) {
-            if (produto.id == buscaId) {
-                produto.nome = "pelicula renomeada";
-                produto.preco = 10.88;
-                produto.quantidade = 100;
-
-                atualizado = true;
-
-            }
-        }
-
-        if (!atualizado) {
-            System.out.println("Id não encontrado");
-        }
-
-        int idDeletar = 2;
-
-        boolean removido = listaProdutos.removeIf(produto -> produto.id == idDeletar);
-
-        if (!removido) {
-            System.out.println("id Não encontrado para remoção");
-        }
-
-        for (Produto produto : listaProdutos) {
-            produto.exibirDetalhes();
-        }
     }
 
 }
---
+++ crud-java/ProdutoService.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoService {
+
+    List<Produto> listaProdutos = new ArrayList<>();
+
+    public void adicionarProduto(Produto produto) {
+        listaProdutos.add(produto);
+
+    }
+
+    public void buscaProduto(String nome) {
+        boolean produtoEncontrado = false;
+
+        for (Produto encontraProduto : listaProdutos) {
+            if (encontraProduto.nome.equals(nome)) {
+                System.out.println("Produto encontrado " + encontraProduto.nome + encontraProduto.id
+                        + encontraProduto.preco + encontraProduto.quantidade);
+                produtoEncontrado = true;
+            }
+        }
+
+        if (!produtoEncontrado) {
+            System.out.println("Produto não encontrado.");
+        }
+    }
+
+    public void atualizaProduto(int idRecebido) {
+
+        boolean idAtualizado = false;
+
+        for (Produto atualizaproduto : listaProdutos) {
+            if (idRecebido == atualizaproduto.id) {
+
+                atualizaproduto.nome = "Caneta azul foi renomeada e passa a ser rosa";
+                idAtualizado = true;
+            }
+
+        }
+
+        if (!idAtualizado) {
+            System.out.println("Id não encontrado");
+        }
+
+    }
+
+    public void deleteProduto(int idDeletar) {
+        boolean removido = listaProdutos.removeIf(produto -> produto.id == idDeletar);
+        if (!removido) {
+            System.out.println("Id Não encontrado");
+        }
+    }
+
+}
