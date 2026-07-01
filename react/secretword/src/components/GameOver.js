c8d38c1455616baca51979f8ee35ff5fc110d352
Caio de Souza Silva <caio.silva@lojastorra.com.br>
2026-07-01 11:35:49 -0300

    feat: adicionando todos estados

--- react/secretword/src/components/GameOver.js
+++ react/secretword/src/components/GameOver.js
@@ -1,11 +1,12 @@
 import "./GameOver.css";
 
-const GameOver = () => {
+const GameOver = ({ retry }) => {
 	return (
 		<div className="start">
-			<h1>Secret Word</h1>
-			<p>Clique no botão abaixo para começar a jogar</p>
-			<button>Começar o Jogo</button>
+			<h1>Game Over</h1>
+						<button onClick={retry}>Recomeçar o jogo</button>
+
+			
 		</div>
 	);
 };
