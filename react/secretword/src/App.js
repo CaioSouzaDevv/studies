f7845d5dce9a8eee07cff5c61a76d7ad8f920b18
Caiosouzadevv <caioss20.cs@gmail.com>
2026-08-11 17:17:07 -0300

    feat: new component game

--- react/secretword/src/App.js
+++ react/secretword/src/App.js
@@ -4,63 +4,13 @@ import { useCallback, useEffect, useState } from "react";
 
 import { wordsList } from "./data/words";
 
-import StartScreen from "./components/StartScreen";
+// import StartScreen from "./components/StartScreen";
 import Game from "./components/Game";
-import GameOver from "./components/GameOver";
+// import GameOver from "./components/GameOver";
 
-const stages = [
-	{ id: 1, name: "start" },
-	{ id: 2, name: "game" },
-	{ id: 3, name: "end" },
-];
 
-function App() {
-	const [words] = useState(wordsList);
+function App() { 
 
-	const [gameStage, setGameStage] = useState(stages[0].name);
-	const [pickedWord, setPickedWord] = useState("");
-	const [pickedCategory, setPickedCategory] = useState("");
-	const [letters, setLetters] = useState("[]");
-
-	const pickWordAndCategory = () => {
-		const categories = Object.keys(words);
-		const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
-		console.log(category);
-
-		const word = words[category][Math.floor(Math.random() * words[category].length)];
-
-		return { word, category };
-	};
-	const startGame = () => {
-		setGameStage(stages[1].name);
-		const { word, category } = pickWordAndCategory();
-
-		let wordLetters = word.split("");
-
-		wordLetters = wordLetters.map((l) => l.toLowerCase());
-
-		console.log(word, category);
-		console.log(wordLetters);
-
-		setPickedWord(word);
-		setPickedCategory(category);
-		setLetters(letters);
-	};
-
-	const verifyLetter = () => {
-		setGameStage(stages[2].name);
-	};
-
-	const retry = () => {
-		setGameStage(stages[0].name);
-	};
-	return (
-		<div className="App">
-			{gameStage === "start" && <StartScreen startGame={startGame} />}
-			{gameStage === "game" && <Game verifyLetter={verifyLetter} />}
-			{gameStage === "end" && <GameOver retry={retry} />}
-		</div>
-	);
+return <Game />
 }
-
 export default App;
--- react/secretword/src/components/Game.js
+++ react/secretword/src/components/Game.js
@@ -1,10 +1,32 @@
 import "./Game.css";
-
+import trofeu from "../assets/game/trofeu.png";
+import coracao from "../assets/game/coracao.png";
 const Game = ({ verifyLetter }) => {
 	return (
-		<div className="game">
-			
-		
+		<div className="wrapper">
+			<div className="game">
+				<div className="game__header">
+					<div className="game__box-points">
+						<img src={trofeu} className="game__header-icon" alt="" />
+						<div className="game__points">
+							<p>Pontos</p>
+							<p>1250</p>
+						</div>
+					</div>
+
+					<div>
+						<h1>SECRET WORLD</h1>
+					</div>
+
+					<div className="game__tentativas">
+						<img className="game__header-icon" src={coracao} width={10} alt="" />
+						<div>
+							<p>Tentativas</p>
+							<span>6/6</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
